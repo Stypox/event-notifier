9c8037a8a95307245aec7726689bc9a04724e2aa
Stypox <stypox@pm.me>
2019-02-18 19:59:16 +0100

    Fixed leaking memory of M_EventFunctions.
    Made pointers to M_EventFunctionBase std::unique_ptr inside EventNotifier's map of functions.
    Added virtual destructor to M_EventFunctionBase.

--- include/stypox/event_notifier.h
+++ include/stypox/event_notifier.h
@@ -2,32 +2,35 @@
 #define _STYPOX_EVENT_NOTIFIER_H_
 #include <functional>
 #include <map>
+#include <memory>
 
 namespace stypox {
 	class M_EventFunctionBase {
 	public:
+		virtual ~M_EventFunctionBase() {};
 		virtual void call(void* data) = 0;
 	};
 
 	template<class R, class A>
 	class M_EventFunction : public M_EventFunctionBase {
 	public:
-		std::function<R(A)> m_function;
-		void call(void* data) override { m_function(*static_cast<argument_type*>(data)); }
-
 		using argument_type = A;
+		std::function<R(argument_type)> m_function;
 
-		M_EventFunction(std::function<R(A)> function) :
+		~M_EventFunction() override {};
+		M_EventFunction(std::function<R(argument_type)> function) :
 			m_function{function} {}
+		void call(void* data) override
+			{ m_function(*static_cast<argument_type*>(data)); }
 	};
 
 	class EventNotifier {
-		std::map<size_t, std::map<size_t, std::vector<M_EventFunctionBase*>>> m_functions;
+		std::map<size_t, std::map<size_t, std::vector<std::unique_ptr<M_EventFunctionBase>>>> m_functions;
 	public:
 		template<class T, class F>
 		void connect(T event, F function) {
 			auto eventFunction = new M_EventFunction{std::function{function}};
-			m_functions[typeid(event).hash_code()][std::hash<T>{}(event)].push_back(eventFunction);
+			m_functions[typeid(event).hash_code()][std::hash<T>{}(event)].push_back(std::unique_ptr<M_EventFunctionBase>(eventFunction));
 
 			using argument_type = typename std::remove_pointer_t<decltype(eventFunction)>::argument_type;
 			static_assert(std::is_same_v<T, argument_type>,
