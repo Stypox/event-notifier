2d3c75a5327f38f8d30dcc0ec97d141dacfbcbb8
Stypox <stypox@pm.me>
2019-02-18 21:03:41 +0100

    Added connect() that takes event type and function.
    This makes it possible to connect ALL events of the same type to the same set of functions. For example even if two ClickEvents differ (e.g. in coordinates), the same function can be notified of both of them.

--- include/stypox/event_notifier.h
+++ include/stypox/event_notifier.h
@@ -3,6 +3,7 @@
 #include <functional>
 #include <map>
 #include <memory>
+#include <variant>
 
 namespace stypox {
 	class M_EventFunctionBase {
@@ -20,27 +21,63 @@ namespace stypox {
 		~M_EventFunction() override {};
 		M_EventFunction(std::function<R(argument_type)> function) :
 			m_function{function} {}
-		void call(void* data) override
-			{ m_function(*static_cast<argument_type*>(data)); }
+		void call(void* data) override {
+			m_function(*static_cast<argument_type*>(data));
+		}
 	};
 
 	class EventNotifier {
-		std::map<size_t, std::map<size_t, std::vector<std::unique_ptr<M_EventFunctionBase>>>> m_functions;
+		using functions_t = std::vector<std::unique_ptr<M_EventFunctionBase>>;
+		using hash_to_functions_t = std::map<size_t, functions_t>;
+		std::map<size_t, std::variant<functions_t, hash_to_functions_t>> m_functions;
 	public:
 		template<class T, class F>
 		void connect(T event, F function) {
 			auto eventFunction = new M_EventFunction{std::function{function}};
-			m_functions[typeid(event).hash_code()][std::hash<T>{}(event)].push_back(std::unique_ptr<M_EventFunctionBase>(eventFunction));
-
+			
+			// check that the only @function argument is the same type as @event
 			using argument_type = typename std::remove_pointer_t<decltype(eventFunction)>::argument_type;
 			static_assert(std::is_same_v<T, argument_type>,
-				"The function passed to stypox::EventNotifier::connect must take one argument of the same type of event.");			
+				"The function passed to stypox::EventNotifier::connect must take one argument of the same type of event.");	
+
+			size_t type_hash = typeid(event).hash_code();
+			size_t event_hash = std::hash<T>{}(event);
+
+			// add function to map
+			if (!m_functions.count(type_hash))
+				m_functions.insert({type_hash, hash_to_functions_t{}});
+			std::get<hash_to_functions_t>(m_functions[type_hash])[event_hash]
+				.push_back(std::unique_ptr<M_EventFunctionBase>{eventFunction});
+		}
+		template<class T, class F>
+		void connect(F function) {
+			auto eventFunction = new M_EventFunction{std::function{function}};
+
+			// check that the only @function argument is the same type as @event
+			using argument_type = typename std::remove_pointer_t<decltype(eventFunction)>::argument_type;
+			static_assert(std::is_same_v<T, argument_type>,
+				"The function passed to stypox::EventNotifier::connect must take one argument of the same type of event.");
+			
+			size_t type_hash = typeid(T).hash_code();
+
+			// add function to map
+			if (!m_functions.count(type_hash))
+				m_functions.insert({type_hash, functions_t{}});
+			std::get<functions_t>(m_functions[type_hash])
+				.push_back(std::unique_ptr<M_EventFunctionBase>{eventFunction});
 		}
 
 		template<class T>
 		void notify(T event) {
-			for(auto&& function : m_functions[typeid(event).hash_code()][std::hash<T>{}(event)])
-				function->call(reinterpret_cast<void*>(&event));
+			auto& functions = m_functions[typeid(event).hash_code()];
+			if (std::holds_alternative<functions_t>(functions)) {
+				for(auto&& function : std::get<functions_t>(functions))
+					function->call(reinterpret_cast<void*>(&event));
+			}
+			else {
+				for(auto&& function : std::get<hash_to_functions_t>(functions)[std::hash<T>{}(event)])
+					function->call(reinterpret_cast<void*>(&event));
+			}
 		}
 	};
 }
